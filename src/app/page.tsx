852dc32d3c2e081a2488558ab7b278685a47b530
odd247 <owenmcmahon@Mac-mini.local>
2025-01-07 17:20:01 +1100

    fix: Convert decimals to number in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
           name: info.name,
           symbol: info.symbol,
           total_supply: info.total_supply,
-          decimals: info.decimals
+          decimals: Number(info.decimals)
         }
       }));
 
